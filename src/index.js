37d9cae3d4fd2c2213732c9a697410eb0d9efae4
Leo Mercier <leo@crowdform.co.uk>
2018-10-10 14:52:33 +0100

    inital

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import './styles/main.scss';
+
+import App from './Components/App';
 
 ReactDOM.render(
   <App />,
